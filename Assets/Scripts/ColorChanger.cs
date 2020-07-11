9f1e17c3f2d77ff7102380e896822f0794661f08
stwiley <mrscottwiley@gmail.com>
2020-07-11 00:54:01 -0700

    Color changing button

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public GameObject Obj;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = Obj.GetComponent<Renderer>();
+    }
+
+    public void OnPress()
+    {
+        rend.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
