e7600a9abe261e17da04ec6c39fd40381ec3083c
stwiley <mrscottwiley@gmail.com>
2020-07-10 17:18:12 -0700

    Scale Sliders and Active Button

---
+++ Assets/Scripts/AppearButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearButton : MonoBehaviour
+{
+    public GameObject Obj;
+    public GameObject canv;
+    MeshRenderer mr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = Obj.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    public void OnPress()
+    {
+        mr.enabled = true;
+        canv.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ScaleSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
+
+public class ScaleSlider : MonoBehaviour
+{
+    public Slider xslider;
+    public Slider yslider;
+    public Slider zslider;
+    Vector3 Scale;
+
+    void Start()
+    {
+        xslider = GameObject.Find("xSlider").GetComponent<Slider>();
+        yslider = GameObject.Find("ySlider").GetComponent<Slider>();
+        zslider = GameObject.Find("zSlider").GetComponent<Slider>();
+        Scale = new Vector3(0.5f, 0.5f, 0.5f);
+        transform.localScale = Scale;
+    }
+
+    public void xSlider()
+    {
+        Scale.x = xslider.value;
+        transform.localScale = Scale;
+    }
+
+    public void ySlider()
+    {
+        Scale.y = yslider.value;
+        transform.localScale = Scale;
+    }
+
+    public void zSlider()
+    {
+        Scale.z = zslider.value;
+        transform.localScale = Scale;
+    }
+}
