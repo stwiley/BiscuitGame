46feaccb115fc8e27f73500f9614956d8eb682ef
stwiley <mrscottwiley@gmail.com>
2020-07-12 00:03:24 -0700

    Death plane added

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour
+{
+    public GameObject DeathCanv;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        DeathCanv.SetActive(true);
+    }
+
+    public void OnPress()
+    {
+        SceneManager.LoadScene("OvenScene");
+    }
+
+}
